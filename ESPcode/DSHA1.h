1267c0994fcfb162c2119bf844e04df0c1b5db37
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-03-18 05:13:48 +0700

    Update DSHA1.h

--- ESPcode/DSHA1.h
+++ ESPcode/DSHA1.h
@@ -13,9 +13,9 @@ public:
         initialize(s);
     }
 
-    DSHA1 &write(const uint8_t *data, size_t len) {
-        size_t bufsize = bytes & 63;
-
+    // fix
+    DSHA1 &write(const unsigned char *data, size_t len) {
+        size_t bufsize = bytes % 64;
         if (bufsize && bufsize + len >= 64) {
             memcpy(buf + bufsize, data, 64 - bufsize);
             bytes += 64 - bufsize;
@@ -24,36 +24,30 @@ public:
             transform(s, buf);
             bufsize = 0;
         }
-
         while (len >= 64) {
             transform(s, data);
             bytes += 64;
             data += 64;
             len -= 64;
         }
-
         if (len > 0) {
             memcpy(buf + bufsize, data, len);
             bytes += len;
         }
-
         return *this;
     }
 
-    void finalize(uint8_t hash[OUTPUT_SIZE]) {
-        const uint8_t pad[64] = {0x80};
-        uint8_t sizedesc[8];
-
+    void finalize(unsigned char hash[OUTPUT_SIZE]) {
+        const unsigned char pad[64] = {0x80};
+        unsigned char sizedesc[8];
         writeBE64(sizedesc, bytes << 3);
-
-        write(pad, 1 + ((119 - (bytes & 63)) & 63));
+        write(pad, 1 + ((119 - (bytes % 64)) % 64));
         write(sizedesc, 8);
-
-        writeBE32(hash,     s[0]);
+        writeBE32(hash, s[0]);
         writeBE32(hash + 4, s[1]);
         writeBE32(hash + 8, s[2]);
-        writeBE32(hash + 12,s[3]);
-        writeBE32(hash + 16,s[4]);
+        writeBE32(hash + 12, s[3]);
+        writeBE32(hash + 16, s[4]);
     }
 
     DSHA1 &reset() {
@@ -62,25 +56,52 @@ public:
         return *this;
     }
 
+    // Warmup giữ nguyên
     DSHA1 &warmup() {
-        uint8_t tmp[20];
-        this->write((uint8_t *)"warmupwarmupwa", 20).finalize(tmp);
+        uint8_t warmup[20];
+        this->write((uint8_t *)"warmupwarmupwa", 20).finalize(warmup);
         return *this;
     }
 
 private:
-    uint32_t s[5] __attribute__((aligned(4)));
-    uint8_t  buf[64] __attribute__((aligned(4)));
+    // 
+    //
+    uint32_t s[5];
+    unsigned char buf[64];
     uint64_t bytes;
 
-    // fast macros
-    #define ROL(x,n) (((x) << (n)) | ((x) >> (32-(n))))
-    #define F1(b,c,d) (d ^ (b & (c ^ d)))
-    #define F2(b,c,d) (b ^ c ^ d)
-    #define F3(b,c,d) ((b & c) | (d & (b | c)))
+    // Giữ nguyên const members
+    const uint32_t k1 = 0x5A827999ul;
+    const uint32_t k2 = 0x6ED9EBA1ul;
+    const uint32_t k3 = 0x8F1BBCDCul;
+    const uint32_t k4 = 0xCA62C1D6ul;
 
-    // init
-    inline void initialize(uint32_t *s) {
+    // QUAN TRỌNG: Giữ inline functions
+    uint32_t inline f1(uint32_t b, uint32_t c, uint32_t d) { 
+        return d ^ (b & (c ^ d)); 
+    }
+    
+    uint32_t inline f2(uint32_t b, uint32_t c, uint32_t d) { 
+        return b ^ c ^ d; 
+    }
+    
+    uint32_t inline f3(uint32_t b, uint32_t c, uint32_t d) { 
+        return (b & c) | (d & (b | c)); 
+    }
+
+    // Tối ưu 2: Thêm các left rotate riêng biệt
+    uint32_t inline left5(uint32_t x) { return (x << 5) | (x >> 27); }
+    uint32_t inline left30(uint32_t x) { return (x << 30) | (x >> 2); }
+    uint32_t inline left1(uint32_t x) { return (x << 1) | (x >> 31); }
+
+    // Round function
+    void inline Round(uint32_t a, uint32_t &b, uint32_t c, uint32_t d, uint32_t &e,
+                      uint32_t f, uint32_t k, uint32_t w) {
+        e += left5(a) + f + k + w;  // Dùng left5 thay vì tính toán trực tiếp
+        b = left30(b);               // Dùng left30 thay vì tính toán trực tiếp
+    }
+
+    void initialize(uint32_t *s) {
         s[0] = 0x67452301ul;
         s[1] = 0xEFCDAB89ul;
         s[2] = 0x98BADCFEul;
@@ -88,46 +109,104 @@ private:
         s[4] = 0xC3D2E1F0ul;
     }
 
-    // core
-    void IRAM_ATTR transform(uint32_t *s, const uint8_t *chunk) {
-
+    // fixed
+    void transform(uint32_t *s, const unsigned char *chunk) {
         uint32_t a = s[0], b = s[1], c = s[2], d = s[3], e = s[4];
-        uint32_t w[16];
+        
+        // 16 biến riêng lẻ
+        uint32_t w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15;
 
-        // load (aligned read)
-        for (int i = 0; i < 16; i++) {
-            w[i] = __builtin_bswap32(((uint32_t*)chunk)[i]);
-        }
+        // Vòng 0-15
+        Round(a, b, c, d, e, f1(b, c, d), k1, w0 = readBE32(chunk + 0));
+        Round(e, a, b, c, d, f1(a, b, c), k1, w1 = readBE32(chunk + 4));
+        Round(d, e, a, b, c, f1(e, a, b), k1, w2 = readBE32(chunk + 8));
+        Round(c, d, e, a, b, f1(d, e, a), k1, w3 = readBE32(chunk + 12));
+        Round(b, c, d, e, a, f1(c, d, e), k1, w4 = readBE32(chunk + 16));
+        Round(a, b, c, d, e, f1(b, c, d), k1, w5 = readBE32(chunk + 20));
+        Round(e, a, b, c, d, f1(a, b, c), k1, w6 = readBE32(chunk + 24));
+        Round(d, e, a, b, c, f1(e, a, b), k1, w7 = readBE32(chunk + 28));
+        Round(c, d, e, a, b, f1(d, e, a), k1, w8 = readBE32(chunk + 32));
+        Round(b, c, d, e, a, f1(c, d, e), k1, w9 = readBE32(chunk + 36));
+        Round(a, b, c, d, e, f1(b, c, d), k1, w10 = readBE32(chunk + 40));
+        Round(e, a, b, c, d, f1(a, b, c), k1, w11 = readBE32(chunk + 44));
+        Round(d, e, a, b, c, f1(e, a, b), k1, w12 = readBE32(chunk + 48));
+        Round(c, d, e, a, b, f1(d, e, a), k1, w13 = readBE32(chunk + 52));
+        Round(b, c, d, e, a, f1(c, d, e), k1, w14 = readBE32(chunk + 56));
+        Round(a, b, c, d, e, f1(b, c, d), k1, w15 = readBE32(chunk + 60));
 
-        for (int i = 0; i < 80; i++) {
+        // Vòng 16-19
+        Round(e, a, b, c, d, f1(a, b, c), k1, w0 = left1(w0 ^ w13 ^ w8 ^ w2));
+        Round(d, e, a, b, c, f1(e, a, b), k1, w1 = left1(w1 ^ w14 ^ w9 ^ w3));
+        Round(c, d, e, a, b, f1(d, e, a), k1, w2 = left1(w2 ^ w15 ^ w10 ^ w4));
+        Round(b, c, d, e, a, f1(c, d, e), k1, w3 = left1(w3 ^ w0 ^ w11 ^ w5));
 
-            uint32_t wi;
+        // Vòng 20-35
+        Round(a, b, c, d, e, f2(b, c, d), k2, w4 = left1(w4 ^ w1 ^ w12 ^ w6));
+        Round(e, a, b, c, d, f2(a, b, c), k2, w5 = left1(w5 ^ w2 ^ w13 ^ w7));
+        Round(d, e, a, b, c, f2(e, a, b), k2, w6 = left1(w6 ^ w3 ^ w14 ^ w8));
+        Round(c, d, e, a, b, f2(d, e, a), k2, w7 = left1(w7 ^ w4 ^ w15 ^ w9));
+        Round(b, c, d, e, a, f2(c, d, e), k2, w8 = left1(w8 ^ w5 ^ w0 ^ w10));
+        Round(a, b, c, d, e, f2(b, c, d), k2, w9 = left1(w9 ^ w6 ^ w1 ^ w11));
+        Round(e, a, b, c, d, f2(a, b, c), k2, w10 = left1(w10 ^ w7 ^ w2 ^ w12));
+        Round(d, e, a, b, c, f2(e, a, b), k2, w11 = left1(w11 ^ w8 ^ w3 ^ w13));
+        Round(c, d, e, a, b, f2(d, e, a), k2, w12 = left1(w12 ^ w9 ^ w4 ^ w14));
+        Round(b, c, d, e, a, f2(c, d, e), k2, w13 = left1(w13 ^ w10 ^ w5 ^ w15));
+        Round(a, b, c, d, e, f2(b, c, d), k2, w14 = left1(w14 ^ w11 ^ w6 ^ w0));
+        Round(e, a, b, c, d, f2(a, b, c), k2, w15 = left1(w15 ^ w12 ^ w7 ^ w1));
+        Round(d, e, a, b, c, f2(e, a, b), k2, w0 = left1(w0 ^ w13 ^ w8 ^ w2));
+        Round(c, d, e, a, b, f2(d, e, a), k2, w1 = left1(w1 ^ w14 ^ w9 ^ w3));
+        Round(b, c, d, e, a, f2(c, d, e), k2, w2 = left1(w2 ^ w15 ^ w10 ^ w4));
+        Round(a, b, c, d, e, f2(b, c, d), k2, w3 = left1(w3 ^ w0 ^ w11 ^ w5));
 
-            if (i < 16) {
-                wi = w[i];
-            } else {
-                wi = ROL(
-                    w[(i-3)&15] ^ w[(i-8)&15] ^ w[(i-14)&15] ^ w[i&15],
-                    1
-                );
-                w[i & 15] = wi;
-            }
+        // Vòng 36-39
+        Round(e, a, b, c, d, f2(a, b, c), k2, w4 = left1(w4 ^ w1 ^ w12 ^ w6));
+        Round(d, e, a, b, c, f2(e, a, b), k2, w5 = left1(w5 ^ w2 ^ w13 ^ w7));
+        Round(c, d, e, a, b, f2(d, e, a), k2, w6 = left1(w6 ^ w3 ^ w14 ^ w8));
+        Round(b, c, d, e, a, f2(c, d, e), k2, w7 = left1(w7 ^ w4 ^ w15 ^ w9));
 
-            uint32_t f, k;
+        // Vòng 40-59
+        Round(a, b, c, d, e, f3(b, c, d), k3, w8 = left1(w8 ^ w5 ^ w0 ^ w10));
+        Round(e, a, b, c, d, f3(a, b, c), k3, w9 = left1(w9 ^ w6 ^ w1 ^ w11));
+        Round(d, e, a, b, c, f3(e, a, b), k3, w10 = left1(w10 ^ w7 ^ w2 ^ w12));
+        Round(c, d, e, a, b, f3(d, e, a), k3, w11 = left1(w11 ^ w8 ^ w3 ^ w13));
+        Round(b, c, d, e, a, f3(c, d, e), k3, w12 = left1(w12 ^ w9 ^ w4 ^ w14));
+        Round(a, b, c, d, e, f3(b, c, d), k3, w13 = left1(w13 ^ w10 ^ w5 ^ w15));
+        Round(e, a, b, c, d, f3(a, b, c), k3, w14 = left1(w14 ^ w11 ^ w6 ^ w0));
+        Round(d, e, a, b, c, f3(e, a, b), k3, w15 = left1(w15 ^ w12 ^ w7 ^ w1));
+        Round(c, d, e, a, b, f3(d, e, a), k3, w0 = left1(w0 ^ w13 ^ w8 ^ w2));
+        Round(b, c, d, e, a, f3(c, d, e), k3, w1 = left1(w1 ^ w14 ^ w9 ^ w3));
+        Round(a, b, c, d, e, f3(b, c, d), k3, w2 = left1(w2 ^ w15 ^ w10 ^ w4));
+        Round(e, a, b, c, d, f3(a, b, c), k3, w3 = left1(w3 ^ w0 ^ w11 ^ w5));
+        Round(d, e, a, b, c, f3(e, a, b), k3, w4 = left1(w4 ^ w1 ^ w12 ^ w6));
+        Round(c, d, e, a, b, f3(d, e, a), k3, w5 = left1(w5 ^ w2 ^ w13 ^ w7));
+        Round(b, c, d, e, a, f3(c, d, e), k3, w6 = left1(w6 ^ w3 ^ w14 ^ w8));
+        Round(a, b, c, d, e, f3(b, c, d), k3, w7 = left1(w7 ^ w4 ^ w15 ^ w9));
+        Round(e, a, b, c, d, f3(a, b, c), k3, w8 = left1(w8 ^ w5 ^ w0 ^ w10));
+        Round(d, e, a, b, c, f3(e, a, b), k3, w9 = left1(w9 ^ w6 ^ w1 ^ w11));
+        Round(c, d, e, a, b, f3(d, e, a), k3, w10 = left1(w10 ^ w7 ^ w2 ^ w12));
+        Round(b, c, d, e, a, f3(c, d, e), k3, w11 = left1(w11 ^ w8 ^ w3 ^ w13));
 
-            if (i < 20)      { f = F1(b,c,d); k = 0x5A827999; }
-            else if (i < 40) { f = F2(b,c,d); k = 0x6ED9EBA1; }
-            else if (i < 60) { f = F3(b,c,d); k = 0x8F1BBCDC; }
-            else             { f = F2(b,c,d); k = 0xCA62C1D6; }
-
-            uint32_t temp = ROL(a,5) + f + e + k + wi;
-
-            e = d;
-            d = c;
-            c = ROL(b,30);
-            b = a;
-            a = temp;
-        }
+        // Vòng 60-79
+        Round(a, b, c, d, e, f2(b, c, d), k4, w12 = left1(w12 ^ w9 ^ w4 ^ w14));
+        Round(e, a, b, c, d, f2(a, b, c), k4, w13 = left1(w13 ^ w10 ^ w5 ^ w15));
+        Round(d, e, a, b, c, f2(e, a, b), k4, w14 = left1(w14 ^ w11 ^ w6 ^ w0));
+        Round(c, d, e, a, b, f2(d, e, a), k4, w15 = left1(w15 ^ w12 ^ w7 ^ w1));
+        Round(b, c, d, e, a, f2(c, d, e), k4, w0 = left1(w0 ^ w13 ^ w8 ^ w2));
+        Round(a, b, c, d, e, f2(b, c, d), k4, w1 = left1(w1 ^ w14 ^ w9 ^ w3));
+        Round(e, a, b, c, d, f2(a, b, c), k4, w2 = left1(w2 ^ w15 ^ w10 ^ w4));
+        Round(d, e, a, b, c, f2(e, a, b), k4, w3 = left1(w3 ^ w0 ^ w11 ^ w5));
+        Round(c, d, e, a, b, f2(d, e, a), k4, w4 = left1(w4 ^ w1 ^ w12 ^ w6));
+        Round(b, c, d, e, a, f2(c, d, e), k4, w5 = left1(w5 ^ w2 ^ w13 ^ w7));
+        Round(a, b, c, d, e, f2(b, c, d), k4, w6 = left1(w6 ^ w3 ^ w14 ^ w8));
+        Round(e, a, b, c, d, f2(a, b, c), k4, w7 = left1(w7 ^ w4 ^ w15 ^ w9));
+        Round(d, e, a, b, c, f2(e, a, b), k4, w8 = left1(w8 ^ w5 ^ w0 ^ w10));
+        Round(c, d, e, a, b, f2(d, e, a), k4, w9 = left1(w9 ^ w6 ^ w1 ^ w11));
+        Round(b, c, d, e, a, f2(c, d, e), k4, w10 = left1(w10 ^ w7 ^ w2 ^ w12));
+        Round(a, b, c, d, e, f2(b, c, d), k4, w11 = left1(w11 ^ w8 ^ w3 ^ w13));
+        Round(e, a, b, c, d, f2(a, b, c), k4, w12 = left1(w12 ^ w9 ^ w4 ^ w14));
+        Round(d, e, a, b, c, f2(e, a, b), k4, left1(w13 ^ w10 ^ w5 ^ w15));
+        Round(c, d, e, a, b, f2(d, e, a), k4, left1(w14 ^ w11 ^ w6 ^ w0));
+        Round(b, c, d, e, a, f2(c, d, e), k4, left1(w15 ^ w12 ^ w7 ^ w1));
 
         s[0] += a;
         s[1] += b;
@@ -136,16 +215,16 @@ private:
         s[4] += e;
     }
 
-    // endian
-    static inline uint32_t readBE32(const uint8_t *ptr) {
+    // Endian conversion
+    uint32_t static inline readBE32(const unsigned char *ptr) {
         return __builtin_bswap32(*(uint32_t *)ptr);
     }
 
-    static inline void writeBE32(uint8_t *ptr, uint32_t x) {
+    void static inline writeBE32(unsigned char *ptr, uint32_t x) {
         *(uint32_t *)ptr = __builtin_bswap32(x);
     }
 
-    static inline void writeBE64(uint8_t *ptr, uint64_t x) {
+    void static inline writeBE64(unsigned char *ptr, uint64_t x) {
         *(uint64_t *)ptr = __builtin_bswap64(x);
     }
 };
